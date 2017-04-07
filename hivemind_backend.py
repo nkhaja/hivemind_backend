82b193da2f08f2c63fa2c82212990f344c3e9b91
nkhaja <nbl.khaja@gmail.com>
2017-04-07 13:51:39 -0700

    updated relay route

--- hivemind_backend.py
+++ hivemind_backend.py
@@ -31,10 +31,13 @@ from_key = 'From'
 # hive keys
 hive_name_key = 'hive_name'
 hive_token_key = 'hive_token'
+drones_key = 'drones'
 date_created_key = 'date_created'
 
 # drone keys
 number_key = 'number'
+last_request_key = 'last_request'
+last_response_key = 'last_response'
 
 ## signal keys
 command_key = 'command'
@@ -138,20 +141,24 @@ def send_messages(numbers, message):
 def create_drones(numbers, hive_id):
     drones = []
     for num in numbers:
-        drone = {:}
-        drone[numbers_key] = num
-        drone[last_request_key] = 'empty'
-        drone[hive_token_key] = hive_id
+        if drones.find_one({number_key:num}) is None:
+            drone = {:}
+            drone[numbers_key] = num
+            drone[last_request_key] = 'empty'
+            drone[last_response_key] = 'empty'
+            drone[hive_token_key] = hive_id
 
-        # insert the drone and get its id
-        drone_id = drones.insert_one(drone)
+            # insert the drone and get its id
+            drone_id = drones.insert_one(drone)
 
-        #get relevant hive and update its drones property
-        hives.find_one({id_key: ObjectId(hive_id)})
+            # get relevant hive and update its drones property
+            hives.update_one({id_key: ObjectId(hive_id)}, { '$addToSet':{drones_key:drone_id} })
 
-        
+# change the last_request of each drone to most recent message
+def update_drones(numbers, message):
+    for num in numbers:
+        drones.update_one{{number_key: num }, $'set'{last_request_key: message}}
 
-        allTasks.update_one({selfId:ObjectId(someTaskId)}, {'$set':newTask})
 
 
 
@@ -175,7 +182,7 @@ def make_error_response(missing_params):
 # TODO: Set these tokens to expire after a period of time.
 
 @app.route('/hives')
-def get_token_for_hive:
+def get_token_for_hive():
 
     missing = validate_params([hive_name_key, date_created_key], request.args)
     if missing:
@@ -183,7 +190,7 @@ def get_token_for_hive:
 
     hive_name = request.args.get(hive_name_key)
     date_created = request.args.get(date_created_key)
-    hive_name_dict = {hive_name_key: hive_name, date_created_key: date_created}
+    hive_name_dict = {hive_name_key: hive_name, date_created_key: date_created, drones_key: []}
     hive_id = hives.insert_one(hive_name_dict).id
 
     # add the token to the dict if accepted
@@ -206,23 +213,30 @@ Assume:
 
 #TODO: Authentication on this route
 @app.route('/signals')
-def send_signal():
+def send_signal):
 
-    missing = validate_params([hive_name_key, date_created_key], request.args)
-    if missing:
-        return make_error_response(missing)
+    # check for missing values
+    missing_body = validate_params([command_key, options_key], request.form)
+    missing_params = validate_params([hive_token], request.args)
+    if missing_body or missing_params:
+        return make_error_response(missing_body + missing_params)
 
-
+    #assign desired values to vars
     body = request.form
     command = body[command_key]
     options_data = body[options_key]
     options = parse_options(options_data)
+    hive_id = request.args.get(hive_token)
 
     #TODO: Check that the string format of these numbers is okay
+
     numbers = body[numbers_key]
 
     message = command + options
-    update_drones(numbers, message)
+
+    # create new drones if new have appeared, update existing drones
+    create_drones(numbers, message)
+    update_drones(numbers, hive_id)
     send_messages(numbers, message)
 
 
@@ -231,13 +245,26 @@ def send_signal():
 # Relay the messages that come from Twilio here
 @app.route('relay')
 def relay_response():
-    if not authenticate(request.headers):
-        return auth_error
+    # if not authenticate(request.headers):
+    #     return auth_error
 
     # get the desired params from the twilio response
-    missing = validate_params([body_key, date_created_key], request.args)
+    missing = validate_params([from_key, body_key, account_sid_key], request.args)
+    if missing:
+        return make_error_response(missing)
+
+    # get the desired params out of message
+    from_num = request.args.get(from_key)
     body = request.args.get(body_key)
-    account_sid = request.args.get(account_sid_key)
+    account_sid_from_twilio = request.args.get(account_sid_key)
+
+    if not account_sid_from_twilio == account_sid:
+        return jsonify{'error': 'this accountSid does not match'}
+
+    #TODO: check that the drones exists before you update it.
+        # however this should exist if its hitting this endpoint
+    drones.update_one({'number_key': from_num}, { '$set':{last_response_key:body}})
+
 
 
 
